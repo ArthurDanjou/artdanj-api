b71f33e50a1757f4b0b18e736ee1a7176977f1ff
Arthur Danjou <arthurdanjou@outlook.fr>
2021-04-17 14:20:11 +0200

    Add form migration

---
+++ database/migrations/1618661863952_forms.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Forms extends BaseSchema {
+  protected tableName = 'forms'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('email')
+      table.string('subject')
+      table.string('content')
+      table.timestamps(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
