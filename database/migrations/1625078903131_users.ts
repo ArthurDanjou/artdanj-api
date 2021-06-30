a89340e61dbbfdffef6f89baef351149347c895a
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-06-30 21:16:17 +0200

    Working

--- database/migrations/1625078903131_users.ts
+++ database/migrations/1625078903131_users.ts
@@ -5,11 +5,11 @@ export default class Users extends BaseSchema {
   protected tableName = 'users'
 
   public async up () {
-    this.schema.createTable(this.tableName, (table) => {
+    this.schema.createTable(this.tableName, async (table) => {
       table.increments('id').primary()
       table.string('username', 255).notNullable()
       table.string('email', 255).notNullable()
-      table.string('password', 180).defaultTo(this.randomPassword()).notNullable()
+      table.string('password', 180).defaultTo(await this.randomPassword()).notNullable()
       table.boolean('is_confirmed').defaultTo(false).notNullable()
       table.string('remember_me_token').defaultTo(null).nullable()
       table.string('confirmation_token').defaultTo(null).nullable()
@@ -17,15 +17,14 @@ export default class Users extends BaseSchema {
     })
   }
 
-  private randomPassword (): string {
+  private async randomPassword(): Promise<string> {
     let password = ''
     const char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!.:=+-_$*^&@#%ù/àçè()é"'
     const size = 64
     for (let i = 0; i < size; i++) {
       password += char.charAt(Math.random() * char.length)
     }
-    console.log(password)
-    console.log(Hash.make(password))
+    password = await Hash.make(password)
     return password
   }
 
