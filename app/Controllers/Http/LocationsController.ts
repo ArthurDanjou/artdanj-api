8e4bfa178cd3866c9a0bd4cb12e69bdcbb077118
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-07-02 17:49:06 +0200

    Working
    Signed-off-by: Arthur DANJOU <arthurdanjou@outlook.fr>

--- app/Controllers/Http/LocationsController.ts
+++ app/Controllers/Http/LocationsController.ts
@@ -1,16 +1,24 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import Location from "App/Models/Location";
 import LocationValidator from "App/Validators/location/LocationValidator";
 
 export default class LocationsController {
 
   public async get ({ response }: HttpContextContract) {
-    const location = await Location.query().orderBy('since', 'desc').firstOrFail()
-    return response.status(200).send({
-      place: location.place,
-      left: location.left,
-      since: location.since
-    })
+    const location = await Location.query().orderBy('since', 'desc').first()
+    if (location) {
+      return response.status(200).send({
+        location: {
+          place: location.place,
+          left: location.left,
+          since: location.since
+        }
+      })
+    } else {
+      return response.status(200).send({
+        location: 'Location is unknown...'
+      })
+    }
   }
 
   public async store ({ request, response }: HttpContextContract) {
