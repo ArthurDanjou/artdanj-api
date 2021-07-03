c05e72d6643b08dec8eacc8872bc74b0337e5c79
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-07-03 18:36:17 +0200

    Fix migration
    Signed-off-by: Arthur DANJOU <arthurdanjou@outlook.fr>

--- database/migrations/1625146912533_guestbook_messages.ts
+++ database/migrations/1625146912533_guestbook_messages.ts
@@ -11,7 +11,7 @@ export default class GuestbookMessages extends BaseSchema {
         .unsigned()
         .references('users.id')
         .onDelete('CASCADE')
-      table.text('message')
+      table.string('message')
       table.timestamps(true, true)
     })
   }
---
+++ database/migrations/1625330080117_guestbook_messages.ts
@@ -0,0 +1,17 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class GuestbookMessages extends BaseSchema {
+  protected tableName = 'guestbook_messages'
+
+  public async up() {
+    await this.schema.table(this.tableName, (table) => {
+      table.text('message')
+    })
+  }
+
+  public async down() {
+    await this.schema.table(this.tableName, (table) => {
+      table.dropColumn('message')
+    })
+  }
+}
