deed45385b3d55e635d77161fe484eedb33ee721
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-07-20 21:51:19 +0200

    Remove Adonisjs Ally
    Signed-off-by: Arthur DANJOU <arthurdanjou@outlook.fr>

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -6,23 +6,5 @@ Route.group(() => {
 
   Route.post('/login', 'AuthController.login')
   Route.post('/logout', 'AuthController.logout')
-
-  Route.get('/twitter/callback', 'AuthController.twitter')
-  Route.get('/github/callback', 'AuthController.github')
-  Route.get('/google/callback', 'AuthController.google')
-  Route.get('/discord/callback', 'AuthController.discord')
-
-  Route.get('/twitter', async ({ally}) => {
-    return ally.use('twitter').redirect()
-  })
-  Route.get('/github', async ({ally}) => {
-    return ally.use('github').redirect()
-  })
-  Route.get('/google', async ({ally}) => {
-    return ally.use('google').redirect()
-  })
-  Route.get('/discord', async ({ally}) => {
-    return ally.use('discord').redirect()
-  })
 }).prefix('/auth')
 
