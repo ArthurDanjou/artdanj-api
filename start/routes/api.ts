89b22acc40963ced4884696303dd914f4b2e01c6
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-08-12 10:26:13 +0200

    Fix auth error

--- start/routes/api.ts
+++ start/routes/api.ts
@@ -4,13 +4,12 @@ import Application from "@ioc:Adonis/Core/Application";
 Route.get('/discord', 'ProfileController.discord')
 Route.get('/me', 'ProfileController.me')
 Route.get('/stats', 'StatsController.index')
+Route.resource('/locations', 'LocationsController').only(['index', 'store'])
 
 Route.group(() => {
   Route.resource('/users', 'UsersController').except(['edit', 'create'])
 
   Route.resource('/translations', 'TranslationsController').except(['edit', 'create'])
-
-  Route.resource('/locations', 'LocationsController').only(['index', 'store'])
 
   Route.resource('/files', 'FileController').only(['index', 'store', 'destroy'])
   Route.get('/files/:filename', async ({response, params}) => {
