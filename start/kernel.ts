b23eb346d69e9a11d106df597f60430f368eefb3
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-08-20 10:35:42 +0200

    Remove shield

--- start/kernel.ts
+++ start/kernel.ts
@@ -22,7 +22,6 @@ import Server from "@ioc:Adonis/Core/Server";
 
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
-  () => import('@ioc:Adonis/Addons/Shield'),
   () => import('App/Middleware/SilentAuth'),
 ])
 
