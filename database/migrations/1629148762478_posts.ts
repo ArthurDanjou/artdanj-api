27cb7d463580cac14ffa2aff4486bfce3d0f6d3a
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-08-28 22:40:24 +0200

    Add PostColor and content

--- database/migrations/1629148762478_posts.ts
+++ database/migrations/1629148762478_posts.ts
@@ -25,16 +25,6 @@ export default class Posts extends BaseSchema {
         .unsigned()
         .references('files.id')
         .onDelete('CASCADE')
-      table
-        .integer('content_id')
-        .unsigned()
-        .references('translations.id')
-        .onDelete('CASCADE')
-      table
-        .integer('post_color_id')
-        .unsigned()
-        .references('post_colors.id')
-        .onDelete('CASCADE')
       table.timestamps(true, true)
     })
   }
