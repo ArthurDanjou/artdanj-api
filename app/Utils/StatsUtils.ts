bedbf1c59b4fefb9c25bdb871bab2e48b3bd57b2
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-10-08 15:14:34 +0200

    Remove useless semi-colon

--- app/Utils/StatsUtils.ts
+++ app/Utils/StatsUtils.ts
@@ -3,12 +3,12 @@ import CommandsRun from "App/Models/CommandsRun";
 import BuildsRun from "App/Models/BuildsRun";
 
 interface Stats {
-  start: Date;
-  end: Date;
+  start: Date
+  end: Date
 
-  development_seconds: number;
-  commands_ran: number;
-  builds_ran: number;
+  development_seconds: number
+  commands_ran: number
+  builds_ran: number
 }
 
 export async function getDevelopmentHours(start: Date, end: Date): Promise<number> {
@@ -57,8 +57,8 @@ export async function getBuildsRan(start: Date, end: Date): Promise<number> {
 }
 
 export async function fetchStatistics(): Promise<Stats> {
-  const start = new Date("01-01-2000");
-  const end = new Date();
+  const start = new Date("01-01-2000")
+  const end = new Date()
 
   const development_seconds = await getDevelopmentHours(start, end)
   const commands_ran = await getCommandsRan(start, end)
@@ -74,8 +74,8 @@ export async function fetchStatistics(): Promise<Stats> {
 }
 
 export async function fetchMonthlyStatistics(): Promise<Stats> {
-  const start = new Date(new Date().setMonth(new Date().getMonth() - 1));
-  const end = new Date();
+  const start = new Date(new Date().setMonth(new Date().getMonth() - 1))
+  const end = new Date()
 
   const development_seconds = await getDevelopmentHours(start, end)
   const commands_ran = await getCommandsRan(start, end)
@@ -91,8 +91,8 @@ export async function fetchMonthlyStatistics(): Promise<Stats> {
 }
 
 export async function fetchWeeklyStatistics(): Promise<Stats> {
-  const start = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
-  const end = new Date();
+  const start = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000)
+  const end = new Date()
 
   const development_seconds = await getDevelopmentHours(start, end)
   const commands_ran = await getCommandsRan(start, end)
@@ -108,8 +108,8 @@ export async function fetchWeeklyStatistics(): Promise<Stats> {
 }
 
 export async function fetchDailyStatistics(): Promise<Stats> {
-  const start = new Date();
-  const end = new Date();
+  const start = new Date()
+  const end = new Date()
 
   const development_seconds = await getDevelopmentHours(start, end)
   const commands_ran = await getCommandsRan(start, end)
