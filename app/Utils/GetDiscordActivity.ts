c2dee1d06437b21650da8c4cc03b205bba8c070b
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-10-08 15:01:10 +0200

    Remove Discord Activity

--- app/Utils/GetDiscordActivity.ts
+++
@@ -1,6 +0,0 @@
-import axios, {AxiosResponse} from "axios";
-import Env from "@ioc:Adonis/Core/Env";
-
-export async function getDiscordActivity(): Promise<AxiosResponse> {
-  return await axios.get(`https://api.lanyard.rest/v1/users/${Env.get('DISCORD_ID')}`)
-}
