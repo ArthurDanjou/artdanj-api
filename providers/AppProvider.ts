fc467db3ad7627aedae63b622d52503f054f4f7c
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-01-15 19:45:35 +0100

    Activate tasks

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -20,20 +20,29 @@ export default class AppProvider {
     // App is ready
     const StatsTask = await import('App/Tasks/StatsTask')
     const StatesTask = await import('App/Tasks/StatesTask')
+    const CurrentSongTask = await import('App/Tasks/CurrentSongTask')
+    const HistorySongsTask = await import('App/Tasks/HistorySongsTask')
 
     await StatsTask.Activate()
     await StatesTask.Activate()
+    await CurrentSongTask.Activate()
+    await HistorySongsTask.Activate()
 
     Logger.info('Application is ready!')
   }
 
   public async shutdown() {
     // Cleanup, since app is going down
-    const StatsTask = (await import('App/Tasks/StatsTask'))
+    const StatsTask = await import('App/Tasks/StatsTask')
     const StatesTask = await import('App/Tasks/StatesTask')
+    const CurrentSongTask = await import('App/Tasks/CurrentSongTask')
+    const HistorySongsTask = await import('App/Tasks/HistorySongsTask')
 
     await StatsTask.ShutDown()
     await StatesTask.ShutDown()
+    await CurrentSongTask.ShutDown()
+    await HistorySongsTask.ShutDown()
+
     Logger.info('Application is closing. Bye...')
   }
 }
