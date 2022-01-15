53c6375b3ef01e8c6a1577d0e2507e52b02ea2bd
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-01-15 22:22:40 +0100

    debug

--- app/Tasks/CurrentSongTask.ts
+++ app/Tasks/CurrentSongTask.ts
@@ -5,7 +5,6 @@ const MS = 1000
 let taskId
 
 async function SpotifyCurrentListeningWatcher(): Promise<void> {
-  console.log('access: ' + getSpotifyAccount().access)
   if (getSpotifyAccount().access === '') return
   await getCurrentPlayingFromSpotify()
 }
