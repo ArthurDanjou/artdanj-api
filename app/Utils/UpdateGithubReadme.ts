10052ea8bf2d4d9a9bc3433b666b1f3f5961c969
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-01-17 21:46:09 +0100

    Add style for music

--- app/Utils/UpdateGithubReadme.ts
+++ app/Utils/UpdateGithubReadme.ts
@@ -56,8 +56,8 @@ export async function updateGithubReadmeSpotify(): Promise<void> {
     old_song = readme.content.split('<!-- Start Song -->')[1].split('<!-- End Song -->')[0]
     new_song = `
 <img alt="Spotify Cover Image" width="75em" height="75em" src="${current_song.image!.url}" />\n
-${current_song.name} _by_ ${current_song.author}  <br/>
-Listening from *${current_song.device_name}*\n`
+**${current_song.name}** by *${current_song.author}*  <br/>
+Listening from **${current_song.device_name}**\n`
   }
   else {
     old_song = readme.content.split('<!-- Start Song -->')[1].split('<!-- End Song -->')[0]
