9adc979c6027680c2e0f3ac66b8086ac83bbc354
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-01-17 22:02:18 +0100

    Rename class for better composition

--- app/Tasks/StatesTask.ts
+++ app/Tasks/StatesTask.ts
@@ -1,5 +1,5 @@
 import Logger from '@ioc:Adonis/Core/Logger'
-import { fetchDevelopingState } from 'App/Utils/StatesTask'
+import { fetchDevelopingState } from 'App/Utils/StatesUtils'
 
 const MS = 1000 * 2 * 60 // 2 min
 let taskId
